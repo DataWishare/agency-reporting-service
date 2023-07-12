a4855085e5f6c46f5e7aaaff4f89fb8477ff54fd
Jin Zhang <sh3l6or@icloud.com>
2023-07-12 16:40:21 -0700

    implement line chart and date buttons

--- pages/02_💡_Analytics.py
+++ pages/02_💡_Analytics.py
@@ -5,6 +5,7 @@ import altair as alt
 import bootstrap as bs
 # import google_ads_queries.config as gc
 from datetime import date, timedelta, datetime
+import pandas as pd
 
 f"""
 # Browse by Customer
@@ -26,6 +27,25 @@ with tab1:
     col11, col12 = st.columns(2)
 
     dates = create_date_range_selector(col11, col12)
+    yesterday = date.today() - timedelta(1)
+    
+
+    col31, col32, col33, col34, col35, col36 = st.columns(6)
+    if col31.button('Yesterday'):
+        dates = [yesterday, yesterday]
+    if col32.button('Last week'):
+        dates = [yesterday - timedelta(7), yesterday]
+    if col33.button('Last month'):
+        dates = [yesterday - timedelta(7), yesterday]
+    if col34.button('Last Q'):
+        dates = [yesterday - timedelta(7), yesterday]
+    if col35.button('Last year'):
+        dates = [yesterday - timedelta(365), yesterday]
+    if col36.button('Maximum'):
+        min_date = mq.get_stats(bs.meta_ads_accounts['ucm'])['Date start'][0]
+        min_date = datetime.strptime(min_date, "%Y-%m-%d").date()
+        dates = [min_date, yesterday]
+    
     col21, col22, col23, col24 = st.columns(4)
     stats = mq.get_stats(bs.meta_ads_accounts['ucm'], dates)
 
@@ -33,6 +53,10 @@ with tab1:
     col22.metric("Clicks", stats['Clicks'][0])
     col23.metric("CPC", stats['CPC'][0])
     col24.metric("Cost", stats['Cost'][0])
+    
+    stats = mq.get_stats(bs.meta_ads_accounts['ucm'], dates, 1)
+    chart = alt.Chart(stats).mark_line().encode(x='Date start', y='Clicks:Q').properties(width=700)
+    st.altair_chart(chart)
 
 
 with tab2:
